899e2237992c031bbab8a846c310c793d70b6cef
SebastianRiosSanchez <sebastianrios47@gmail.com>
2024-07-08 11:16:32 -0500

    Ajustes clase service

--- src/main/java/com/example/demo/infrastructure/dto/TestDto.java
+++ src/main/java/com/example/demo/infrastructure/dto/TestDto.java
@@ -9,32 +9,32 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class TestDto  implements  iTestDto{
+public class TestDto implements iTestDto {
 
-private final TestRepository repository;
+    private final TestRepository repository;
 
-public TestDto( TestRepository repository ) {
-this.repository = repository;
-}
+    public TestDto(TestRepository repository) {
+        this.repository = repository;
+    }
 
-@Override
-public Test save(Test test) {
-return repository.save(test);
-}
+    @Override
+    public Test save(Test test) {
+        return repository.save(test);
+    }
 
-@Override
-public List<Test> findAll() {
-return repository.findAll();
-}
+    @Override
+    public List<Test> findAll() {
+        return repository.findAll();
+    }
 
-@Override
-public Optional<Test> findById(Long id) {
-return repository.findById(id);
-}
+    @Override
+    public Optional<Test> findById(Long id) {
+        return repository.findById(id);
+    }
 
-@Override
-public void deleteById(Long id) {
-repository.deleteById(id);
-}
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 
 }
